0d1d9c62d61c31c8c1e5949e8d54ccaaf8460088
zizom01 <khmes.alajme@gmail.com>
2024-06-02 22:18:18 +0300

    added warning popup to remove entries button. still needs functionality to be added.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const perc = 0.35;
 const price = document.getElementById("price");
-
+const result = document.getElementById("list");
+let field = document.getElementById("field");
+let hasH2 = false;
 
 
 function addtoprice() {
@@ -10,13 +12,9 @@ function addtoprice() {
     makep.textContent = +actual + +pricevalue;
     result.appendChild(makep);
 }
-const result = document.getElementById("list");
-let field = document.getElementById("field");
-let hasH2 = false;
+
 
 function haveh2() {
-
-
     // Loop through each child element
 for (let i = 0; i < result.children.length; i++) {
     let child = result.children[i];
@@ -33,6 +31,8 @@ if (hasH2 == true) {
     field.classList.add("has-child");
 }
 }
+
+
 function removeh2() {
        while (result.firstChild) {
         result.removeChild(result.firstChild);
